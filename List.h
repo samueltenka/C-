a5e8c61ac35d24f5ab0aa439436bf3963ad74e16
samueltenka <iguavadon@gmail.com>
2014-08-10 00:26:19 -0400

    Create List.h

---
+++ List.h
@@ -0,0 +1,22 @@
+//
+//Linked List:
+
+
+#ifndef LIST
+#define LIST
+template<typename T>
+struct LNode {
+	T* val;
+	LNode* next, prev;
+};
+
+
+template<typename T>
+class List {
+};
+
+
+template<typename T>
+class LIterator {
+};
+#endif
