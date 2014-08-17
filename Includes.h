017596ff9a5449b82302137fbb3e8cfaec841788
samueltenka <iguavadon@gmail.com>
2014-08-17 02:00:15 -0400

    Create Includes.h
    includes NULL and basic debugging helper: ERROR(printf-style args)

---
+++ Includes.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDES
+#define INCLUDES
+
+#include <cstdlib> // for NULL
+
+#include <cstdio>
+#define ERROR(X) do {printf("ERROR: "); printf(X); printf("\n"); scanf("");} while(0)
+
+#endif INCLUDES
