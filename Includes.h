d7649792b75ce9f6c88765ed30420472cd884a59
samueltenka <iguavadon@gmail.com>
2014-08-17 03:20:28 -0400

    Update Includes.h
    added STATUS

--- Includes.h
+++ Includes.h
@@ -9,4 +9,6 @@
 #define ERROR(X) do {printf("ERROR: "); printf(X); printf("\n"); \
 						char error_l; std::cin >> error_l;} while(0)
 
+#define STATUS(func_name) printf("STATUS: %s\n", func_name)
+
 #endif INCLUDES
